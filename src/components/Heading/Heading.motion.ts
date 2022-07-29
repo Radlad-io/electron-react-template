167819fabe5501d415abd03a9e5663823f211a90
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-07-29 15:32:36 -0400

    Config

---
+++ src/components/Heading/Heading.motion.ts
@@ -0,0 +1,19 @@
+const variants = {
+  initial: {
+    y: 65,
+  },
+  in: {
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  out: {
+    y: 65,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+export default variants;
